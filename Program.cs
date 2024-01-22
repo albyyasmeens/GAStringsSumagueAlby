7336e5bd550dcd35503baf11a5a2851ad446b154
albyyasmeens <156489829+albyyasmeens@users.noreply.github.com>
2024-01-21 19:17:34 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        string myFirstName = "Alby-Yasmeen";
+        Console.WriteLine(myFirstName);
+        string myLastName = "Sumague";
+        Console.WriteLine(myLastName);
+        string myFullName = myFirstName + " " + myLastName;
+        Console.WriteLine(myFullName);
+        string myAge = "27";
+        string whoAmI = "My name is " + myFullName + "." + " I am " + myAge + " years old."+ "and I am a student at RTC.";
+                Console.WriteLine(whoAmI);
+            }
+        }
+
+
